0901334db4456832841070f7afec5dca240147b1
jtaylor179 <jtaylor179@hotmail.com>
2021-04-18 12:40:01 -0400

    1 file modified

--- fire.js
+++ fire.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
-var config = {
-    apiKey: "AIzaSyDNm6XjpKWmE5fb6y19ITJByPLunMnpSPI",
-    authDomain: "test-e847f.firebaseapp.com",
-    databaseURL: "https://test-e847f.firebaseio.com",
-    projectId: "test-e847f",
-    storageBucket: "test-e847f.appspot.com",
-    messagingSenderId: "937619158003"
+const firebaseConfig = {
+  apiKey: "AIzaSyAP1MICsZ1gdOR2BVZrP4LNaXuxVZrRVUM",
+  authDomain: "friendly-chat-7abf1.firebaseapp.com",
+  projectId: "friendly-chat-7abf1",
+  storageBucket: "friendly-chat-7abf1.appspot.com",
+  messagingSenderId: "702879392834",
+  appId: "1:702879392834:web:6f5e78a8a7519633ecf9db"
 };
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
