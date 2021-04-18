8628a8059a9cbfadd9a55d86429b87dfeb5ba6da
jtaylor179 <jtaylor179@hotmail.com>
2021-04-18 12:42:48 -0400

    2 files modified

--- fire.js
+++ fire.js
@@ -1,4 +1,14 @@
-import firebase from 'firebase'
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyAP1MICsZ1gdOR2BVZrP4LNaXuxVZrRVUM",
   authDomain: "friendly-chat-7abf1.firebaseapp.com",
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import firebase from './fire.js';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import firebase from "./fire.js";
 
 class App extends Component {
   constructor() {
@@ -10,11 +10,8 @@ class App extends Component {
       loading: false
     };
 
-
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
- 
-    
   }
 
   componentDidMount() {
@@ -24,15 +21,15 @@ class App extends Component {
     //set loading to true when fetching data for authentication
     this.setState({ loading: true });
 
-    firebase.auth().onAuthStateChanged((user) => {
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.setState({ user });
-      } 
+      }
       //when data is loaded, set loading to false
       this.setState({ loading: false });
     });
 
-  /*
+    /*
     firebase.auth().getRedirectResult().then( function (result){
       // This gives you a Google Access Token. You can use it to access the Google API.
       const token = result.credential.accessToken;
@@ -52,65 +49,66 @@ class App extends Component {
         const credential = error.credential;
         // ...
         
-    });
+    }); */
   }
-  */
 
-  login(){
+  login() {
     //console.log("sign-in");
-   
 
     //This code will setup the Google login page
     const provider = new firebase.auth.GoogleAuthProvider();
 
     //reloads the page, and tries to sign in
     //when you reload the page, componentDidMount() will execute
-    firebase.auth().signInWithRedirect(provider);
+    // firebase.auth().signInWithRedirect(provider);
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
   }
 
   logout() {
     //This will sign out of firebase authentication and set
     //the user variable to null
-    firebase.auth().signOut()
-    .then(() => {
-      this.setState({
-        user: null
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
       });
-    });
   }
 
   render() {
-    if(this.state.loading){
-      return (
-        <div> Loading... </div>
-      )
-    }else if( this.state.user == null){
+    if (this.state.loading) {
+      return <div> Loading... </div>;
+    } else if (this.state.user == null) {
       return (
         <div>
-          
-          Click below to Login with your Google Account <br /><br />
-          <button onClick={this.login}>Log In</button> 
+          Click below to Login with your Google Account <br />
+          <br />
+          <button onClick={this.login}>Log In</button>
           <br />
         </div>
-       
-      )
+      );
     } else {
       return (
         <div>
-                <button onClick={this.logout}>Log Out</button>
-                <br/><br/>
-                Hello {this.state.user.displayName}, you are logged in!
-                <br/><br />
-                Your Google email is { this.state.user.email }
-                <br /><br />
-                Your Google Photo ID is: <br />
-                <img src={this.state.user.photoURL} width='100px'/>
-                <br />
-                
+          <button onClick={this.logout}>Log Out</button>
+          <br />
+          <br />
+          Hello {this.state.user.displayName}, you are logged in!
+          <br />
+          <br />
+          Your Google email is {this.state.user.email}
+          <br />
+          <br />
+          Your Google Photo ID is: <br />
+          <img src={this.state.user.photoURL} width="100px" />
+          <br />
         </div>
       );
     }
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
